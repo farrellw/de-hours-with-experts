fffba31a72e1f3bb3bcbc74a1a95378e3979f64b
Will Farrell <farrell.will.g@gmail.com>
2020-05-17 19:55:50 -0500

    Solve using 'Permutations'

--- scala/src/main/scala/com/labs1904/NextBiggestNumber.scala
+++ scala/src/main/scala/com/labs1904/NextBiggestNumber.scala
@@ -2,16 +2,31 @@ package com.labs1904
 
 object NextBiggestNumber {
 
-  def main(args: Array[String]) = {
+  def main(args: Array[String]): Unit = {
 
-    val input = args(0).toInt
+    val input = 67809
     val nextBiggestNumber = getNextBiggestNumber(input)
     println(s"Input: $input")
     println(s"Next biggest number: $nextBiggestNumber")
   }
 
-  def getNextBiggestNumber(i: Integer) = {
-    //TODO: Implement me!
-    0
+  def permutationsOfInt(i: Integer): List[Int] = {
+    val s = i.toString
+    val a = s.split("")
+    a.permutations.toList.map(_.foldLeft("")((agg, next) => {
+      agg + next
+    })).map(_.toInt)
+  }
+
+  def getNextBiggestNumber(i: Integer): Int = {
+    val perms = permutationsOfInt(i)
+
+    perms.foldLeft(-1)((agg, next) => {
+      if((next < agg || agg == -1) && next > i){
+        next
+      } else {
+        agg
+      }
+    })
   }
 }
